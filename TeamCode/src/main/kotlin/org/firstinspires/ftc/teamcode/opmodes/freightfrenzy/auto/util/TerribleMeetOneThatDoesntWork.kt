5f5649c4b2103fccdd0fc2aeb1944f872cf0f6d7
Andrew Craig <andrewdavidcraig@gmail.com>
2022-12-01 18:01:22 -0500

    Added prototypes (actual prototypes not code prototypes) for poses on the blue left auto.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/freightfrenzy/auto/util/TerribleMeetOneThatDoesntWork.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmodes/freightfrenzy/auto/util/TerribleMeetOneThatDoesntWork.kt
@@ -6,6 +6,7 @@ import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import org.firstinspires.ftc.teamcode.robot.abstracts.BaseOpMode
+import org.firstinspires.ftc.teamcode.robot.subsystems.ColorCone
 import org.firstinspires.ftc.teamcode.robot.subsystems.drivetrain.trajectorysequence.TrajectorySequence
 
 @Disabled
@@ -16,13 +17,57 @@ class TerribleMeetOneThatDoesntWork : BaseOpMode() {
 
     override fun setup() {
         // Make an array of the poses so we can iterate over them
+        val poses = arrayOf(pose1, pose2, poseIntake, pose3, poseIntake, pose4, pose5, pose6, pose7, pose8, pose9)
+
+        // Dimensions for blue left
+        pose0.x = 31.5
+        pose0.y = 7.5
+
+        // pose1 scores low
+        pose1.x = 48.0
+        pose1.y = 13.0
+        pose1.h = 0.0
+
+        pose2.x = 36.0
+        pose2.y = 50.0
+
+        poseIntake.x = 10.0
+        poseIntake.y = 60.0
+        poseIntake.h = Math.toRadians(90.0)
+
+        // pose3 scores high across blue-left/red-right wall
+        pose3.x = 62.0
+        pose3.y = 48.0
+        pose3.h = Math.PI
+
+        // Add intake exe
+        // pose4 scores high across blue wall
+        pose4.x = 48.0
+        pose4.y = 62.0
+        pose4.h = Math.toRadians(270.0)
+
+        // pose5 avoids obstacles
+        pose5.x = 60.0
+        pose5.y = 60.0
+        pose5.h = Math.toRadians(270.0)
+
+        // pose*colour* parks for vision
+        poseRed.x = 12.0
+        poseRed.y = 60.0
+
+        poseGreen.x = 36.0
+        poseGreen.y = 60.0
+
+        poseBlue.x = 59.0
+        poseBlue.y = 60.0
 
         //val poses = arrayOf(pose1, pose2)
 
         // Initialize a trajectory builder
         val trajBuilder = robot.longSchlong.trajectorySequenceBuilder(pose0.toPose2d())
 
-        /**pose1.y = 1.0
+
+        pose1.y = 1.0
         if (robot.colorCone.leftColor == ColorCone.ConeColor.RED) {
             pose1.x = -28.0
             pose1.h = 20.0
@@ -37,7 +82,7 @@ class TerribleMeetOneThatDoesntWork : BaseOpMode() {
         }
         pose2.y = -50.0
         pose2.x = pose1.x
-        pose2.h = 0.0**/
+        pose2.h = 0.0
 
         pose1.y = -50.0
         pose1.x = 8.0
@@ -57,7 +102,19 @@ class TerribleMeetOneThatDoesntWork : BaseOpMode() {
 
     companion object {
         @JvmField var pose0: Pose = Pose()
+        @JvmField var poseIntake: Pose = Pose()
+        @JvmField var poseRed: Pose = Pose()
+        @JvmField var poseGreen: Pose = Pose()
+        @JvmField var poseBlue: Pose = Pose()
         @JvmField var pose1: Pose = Pose()
+        @JvmField var pose2: Pose = Pose()
+        @JvmField var pose3: Pose = Pose()
+        @JvmField var pose4: Pose = Pose()
+        @JvmField var pose5: Pose = Pose()
+        @JvmField var pose6: Pose = Pose()
+        @JvmField var pose7: Pose = Pose()
+        @JvmField var pose8: Pose = Pose()
+        @JvmField var pose9: Pose = Pose()
     }
 
     class Pose(@JvmField var x: Double = 0.0,
