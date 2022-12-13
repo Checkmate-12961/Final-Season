13f3a61b54a6723f9f46edfa555b423d67498459
ryleu <69326171+ryleu@users.noreply.github.com>
2022-12-12 21:41:42 -0500

    make the robot not shit itself when lifting

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/subsystems/LiftyLinkage.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/subsystems/LiftyLinkage.kt
@@ -3,9 +3,12 @@ package org.firstinspires.ftc.teamcode.robot.subsystems
 import com.acmerobotics.dashboard.config.Config
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.firstinspires.ftc.teamcode.robot.HardwareNames.Motors
 import org.firstinspires.ftc.teamcode.robot.abstracts.AbstractSubsystem
 import org.firstinspires.ftc.teamcode.robot.abstracts.SubsystemMap
+import kotlin.math.abs
+import kotlin.math.sign
 
 @Config
 class LiftyLinkage(hardwareMap: HardwareMap) : AbstractSubsystem {
@@ -23,26 +26,49 @@ class LiftyLinkage(hardwareMap: HardwareMap) : AbstractSubsystem {
         HOLD({ holdPower })
     }
 
-    var speed: Double
-        get() = liftMotorA.power
-        set(value) = motors.forEach { it.power = value }
+    var speed = 0.0
+
+    var currentSpeed = 0.0
+        private set
 
     fun action(action: Action) {
         speed = action.power()
     }
 
+    override fun loop() {
+        // makes the robot not shit itself as badly
+
+        currentSpeed = liftMotorA.power
+
+        val delta = sign(speed - currentSpeed) * adjustmentRate
+        val newPower = if (
+                abs(abs(currentSpeed + delta) - abs(speed)) > adjustmentRate
+            ) currentSpeed + delta
+            else speed
+
+        if (delta != 0.0) {
+            motors.forEach { it.power = newPower }
+        }
+    }
+
+    override fun generateTelemetry(telemetry: Telemetry) {
+        telemetry.addData("liftTargetVel", speed)
+        telemetry.addData("liftRealVel", currentSpeed)
+        telemetry.addData("liftVelDelta", currentSpeed - speed)
+    }
+
     init {
         // Initialize the motors
         motors.forEach { motor ->
-            motor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
             motor.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
             motor.power = 0.0
         }
     }
 
     companion object {
-        @JvmField var upPower = 0.7
-        @JvmField var downPower = -0.3
-        @JvmField var holdPower = 0.1
+        @JvmField var adjustmentRate = 0.03
+        @JvmField var upPower = .6
+        @JvmField var holdPower = .0
+        @JvmField var downPower = -.3
     }
 }
