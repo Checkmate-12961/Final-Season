8a073d98c58d5d7ce2890f28abfb145b0d4fea94
ryleu <69326171+ryleu@users.noreply.github.com>
2023-01-26 23:09:30 -0500

    add tracking wheel to telemetry data

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/subsystems/drivetrain/localizers/TrackingWheelLocalizer.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/robot/subsystems/drivetrain/localizers/TrackingWheelLocalizer.kt
@@ -23,7 +23,11 @@ package org.firstinspires.ftc.teamcode.robot.subsystems.drivetrain.localizers
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer
 import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.firstinspires.ftc.teamcode.robot.HardwareNames
+import org.firstinspires.ftc.teamcode.robot.abstracts.AbstractSubsystem
+import org.firstinspires.ftc.teamcode.robot.abstracts.SubsystemMap
+import org.firstinspires.ftc.teamcode.robot.util.Encoder
 
 /*
  * Sample tracking wheel localizer implementation assuming the standard configuration:
@@ -48,35 +52,45 @@ class TrackingWheelLocalizer(hardwareMap: HardwareMap) : ThreeTrackingWheelLocal
         Pose2d(-LATERAL_DISTANCE / 2.0, 0.0, kotlin.math.PI / 2),  // rear
         Pose2d(0.0, Y_OFFSET, 0.0) // center
     )
-) {
+), AbstractSubsystem {
+    override val tag = this.javaClass.simpleName
+    override val subsystems = SubsystemMap { tag }
+
     private val frontEncoder = HardwareNames.Encoders.FRONT.get(hardwareMap)
     private val rearEncoder = HardwareNames.Encoders.REAR.get(hardwareMap)
     private val centerEncoder = HardwareNames.Encoders.CENTER.get(hardwareMap)
 
-    override fun getWheelPositions(): List<Double> {
-        return listOf(
-            encoderTicksToInches(frontEncoder.currentPosition.toDouble()) * Y_MULTIPLIER,
-            encoderTicksToInches(rearEncoder.currentPosition.toDouble()) * Y_MULTIPLIER,
-            encoderTicksToInches(centerEncoder.currentPosition.toDouble()) * X_MULTIPLIER
-        )
+    private val multipliers = listOf(Y_MULTIPLIER, Y_MULTIPLIER, X_MULTIPLIER)
+
+    private val resetValues = onEachEncoder {
+        encoderTicksToInches(it.currentPosition.toDouble())
     }
 
-    override fun getWheelVelocities(): List<Double> {
-        // DONE: If your encoder velocity can exceed 32767 counts / second (such as the REV Through Bore and other
-        //  competing magnetic encoders), change Encoder.getRawVelocity() to Encoder.getCorrectedVelocity() to enable a
-        //  compensation method
-        return listOf(
-            encoderTicksToInches(frontEncoder.correctedVelocity * Y_MULTIPLIER),
-            encoderTicksToInches(rearEncoder.correctedVelocity * Y_MULTIPLIER),
-            encoderTicksToInches(centerEncoder.correctedVelocity * X_MULTIPLIER)
-        )
+    override fun getWheelPositions(): List<Double> =
+        mulList(onEachEncoder { it.currentPosition.toDouble() }, multipliers)
+
+    override fun getWheelVelocities(): List<Double> =
+        mulList(onEachEncoder(Encoder::getCorrectedVelocity), multipliers)
+
+    private fun <T> onEachEncoder(callback: (Encoder) -> T): List<T> =
+        listOf(frontEncoder, rearEncoder, centerEncoder).map(callback)
+
+    private fun mulList(a: List<Double>, b: List<Double>) = a.zip(b) { x: Double, y: Double ->
+        x * y
+    }
+
+    override fun generateTelemetry(telemetry: Telemetry) {
+        val wheelPositions = getWheelPositions()
+        telemetry.addData("front", wheelPositions[0] - resetValues[0])
+        telemetry.addData("rear", wheelPositions[1] - resetValues[1])
+        telemetry.addData("center", wheelPositions[2] - resetValues[2])
     }
 
     companion object {
         @JvmField var TICKS_PER_REV = 8192.0 // we use REV through bore encoders
         @JvmField var WHEEL_RADIUS = 1.96 / 2 // inches. this is from the andymark listing
         @JvmField var GEAR_RATIO = 1.0 // output (wheel) speed / input (encoder) speed
-        @JvmField var LATERAL_DISTANCE = 10.69 // in; distance between the left and right wheels
+        @JvmField var LATERAL_DISTANCE = 10.11 // in; distance between the left and right wheels
         @JvmField var Y_OFFSET = -3.55 // in; offset of the lateral wheel
         @JvmField var X_MULTIPLIER = 1.0023 // Multiplier in the X direction
         @JvmField var Y_MULTIPLIER = 1.0047 // Multiplier in the Y direction
